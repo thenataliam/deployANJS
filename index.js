da73c55ccff41985c76541b2ef5c89394e6e3322
thenataliam <102542373+thenataliam@users.noreply.github.com>
2023-05-31 20:27:07 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-/* const cors=require("cors"); */
 const express = require("express"); //importar express
 const bodyParser = require("body-parser");
 const app = express(); //crear al servidor
 const port = process.env.PORT || 3000;
+/* const cors=require("cors"); */
 app.use(bodyParser.urlencoded({ extended:false}));
 app.use(bodyParser.json());
 /* app.use(cors); */
