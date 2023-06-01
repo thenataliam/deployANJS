8e5e71ef9ff3703792fdb9f5e9160c69f74a4f8f
thenataliam <102542373+thenataliam@users.noreply.github.com>
2023-05-31 20:05:17 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+/* const cors=require("cors"); */
+const express = require("express"); //importar express
+const bodyParser = require("body-parser");
+const app = express(); //crear al servidor
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
+/* app.use(cors); */
+const usuarios= [
+    {id:0, nombre:'Christopher', correo:'chris@correo.com', mensaje:'Mensaje de Chris'},
+    {id:1, nombre:'Harry', correo:'harry@correo.com', mensaje:'Mensaje de Harry'}
+];
+
+app.get('/metodo/mostrar', (req, res)=> {
+res.send(usuarios);
+});
+app.listen(port, () => {
+ console.log(`hola servidor ejecucion en http://localhost:${port}`);
+})
